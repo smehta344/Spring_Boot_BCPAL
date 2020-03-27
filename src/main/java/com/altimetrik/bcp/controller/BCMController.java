d288d5ec2a7c8cdde148fcd16bbd6e7fc830b7c2
Sathish <snagappan@altimetrik.com>
2020-03-27 15:39:45 +0530

    changes added

--- src/main/java/com/altimetrik/bcp/controller/BCMController.java
+++ src/main/java/com/altimetrik/bcp/controller/BCMController.java
@@ -1,5 +1,6 @@
 package com.altimetrik.bcp.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,9 +12,19 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.altimetrik.bcp.dao.AccountRepo;
+import com.altimetrik.bcp.dao.LeaderRepo;
+import com.altimetrik.bcp.dao.LocationRepo;
+import com.altimetrik.bcp.dao.ProjectAssocRepo;
+import com.altimetrik.bcp.dao.ProjectRepo;
+import com.altimetrik.bcp.entity.Account;
+import com.altimetrik.bcp.entity.Leader;
+import com.altimetrik.bcp.entity.Location;
 import com.altimetrik.bcp.entity.ProjLocLeaderAssoc;
+import com.altimetrik.bcp.entity.Project;
 import com.altimetrik.bcp.model.PlanDetailFormData;
 import com.altimetrik.bcp.service.BCMService;
 
@@ -23,10 +34,32 @@ import com.altimetrik.bcp.service.BCMService;
  *
  */
 @RestController
+@RequestMapping("/bcm")
 public class BCMController {
 	
 	@Autowired
 	BCMService bcmService;
+	
+	@Autowired
+	AccountRepo accountRepo;
+	
+	@Autowired
+	LeaderRepo leaderRepo;
+	
+	@Autowired
+	ProjectAssocRepo asscoRepo;
+	
+	@Autowired
+	LocationRepo locationRepo;
+	
+	@Autowired
+	AccountRepo acRepo;
+	
+	@Autowired
+	ProjectRepo projectRepo;
+
+	@Autowired
+	ProjectAssocRepo projecAssocRepo;
 
 	/*
 	 * createProjectLocationAssociate
@@ -36,38 +69,36 @@ public class BCMController {
 		bcmService.createDilyStatus(formData);
 		
 	}
-
-	/*
-	 * getProjectLocationAssociate
-	 */
-	@GetMapping(value = "/findbcpplan/{projectLocationAssociateId}")
-	public Optional<ProjLocLeaderAssoc> findDatabyId(
-			@PathVariable(" projectLocationAssociateId") int projectLocationAssociateId) {
-
-		Optional<ProjLocLeaderAssoc> projLocLeaderAssoc = null;
-		return projLocLeaderAssoc;
-
+	
+	@GetMapping(value = "/getAllAccounts")
+	public ResponseEntity<List<Account>> getAllLocation(){
+		List<Account> accountList = accountRepo.findAll();
+		return ResponseEntity.ok().body(accountList);
 	}
-
-	/*
-	 * getAllProjectLocationAssociate
-	 */
-	@GetMapping(value = "/findallbcpplan")
-	public List<ProjLocLeaderAssoc> findAllProjectLocationAssociate() {
-		List<ProjLocLeaderAssoc> projLocLeaderAssocslist = null;
-		return projLocLeaderAssocslist;
+	
+	@GetMapping(value = "/getLeader/{accountId}")
+	public Leader getLeaderById(@PathVariable("accountId") int accountId){
+		List<ProjLocLeaderAssoc> assocList = asscoRepo.findAll();
+		
+		for(ProjLocLeaderAssoc leaderAssoc:assocList){
+			if(leaderAssoc.getAccount().getId() == accountId){
+				return leaderAssoc.getLeader();
+			}
+		}
+		return null;
+		
 	}
-
-	/*
-	 * UpdateProjectLocationAssociate
-	 */
-	@PutMapping(value = "/updatebcpplan/{projectLocationAssociateId}")
-	public ResponseEntity<String> updateProjectLocationAssociate(
-			@PathVariable("projectLocationAssociateId") int projectLocationAssociateId,
-			@RequestBody ProjLocLeaderAssoc projLocLeaderAssoc) {
-
-		return new ResponseEntity<String>("UpdateSuccessfully", HttpStatus.OK);
-
+	
+	@GetMapping(value = "/getProject/{accountId}")
+	public ResponseEntity<List<Project>> getProject(@PathVariable("accountId") int accountId){
+		List<ProjLocLeaderAssoc> assocList = projecAssocRepo.findAll();
+		List<Project> projList = new ArrayList<Project>();
+		for(ProjLocLeaderAssoc projAssoc:assocList){
+			if(projAssoc.getAccount().getId() == accountId){
+				projList.add(projAssoc.getProject());
+			}
+		}
+		return ResponseEntity.ok().body(projList);
 	}
 
 }
