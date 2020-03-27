c501cf4b448b5b54cd77491a2d1708f23156596a
Sathish <snagappan@altimetrik.com>
2020-03-27 11:54:40 +0530

    controllers added

--- src/main/java/com/altimetrik/bcp/controller/PlanDetailController.java
+++ src/main/java/com/altimetrik/bcp/controller/PlanDetailController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +18,6 @@ import com.altimetrik.bcp.dao.AccountRepo;
 import com.altimetrik.bcp.dao.LocationRepo;
 import com.altimetrik.bcp.entity.Account;
 //import com.altimetrik.bcp.dao.BcpRepo;
-import com.altimetrik.bcp.model.BusinessContinuityPlan;
 import com.altimetrik.bcp.model.PlanDetailEntity;
 
 @RestController
@@ -30,7 +30,7 @@ public class PlanDetailController {
 	private LocationRepo locationRepo;
 
 	@GetMapping(value = "/getLocationAndAccount")
-	public PlanDetailEntity getLocationaAndAccount(@RequestBody BusinessContinuityPlan businessContinuityPlan) {
+	public PlanDetailEntity getLocationaAndAccount() {
 		 PlanDetailEntity planDetail = new PlanDetailEntity();
 		 planDetail.setAccountData(acRepo.findAll());
 		 planDetail.setLocationData(locationRepo.findAll());
