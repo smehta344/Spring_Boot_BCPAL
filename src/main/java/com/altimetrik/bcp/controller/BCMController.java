1518c950bf7a7aa05055e2f8b87e78af2d23f707
smehta <smehta@AIPL-CHE-LT-604.altimetrik.com>
2020-03-27 19:47:24 +0530

    Some Change

--- src/main/java/com/altimetrik/bcp/controller/BCMController.java
+++ src/main/java/com/altimetrik/bcp/controller/BCMController.java
@@ -33,25 +33,25 @@ import com.altimetrik.bcp.service.BCMService;
 @RestController
 @RequestMapping("/bcm")
 public class BCMController {
-	
+
 	@Autowired
 	BCMService bcmService;
-	
+
 	@Autowired
 	AccountRepo accountRepo;
-	
+
 	@Autowired
 	LeaderRepo leaderRepo;
-	
+
 	@Autowired
 	ProjectAssocRepo asscoRepo;
-	
+
 	@Autowired
 	LocationRepo locationRepo;
-	
+
 	@Autowired
 	AccountRepo acRepo;
-	
+
 	@Autowired
 	ProjectRepo projectRepo;
 
@@ -64,60 +64,54 @@ public class BCMController {
 	@PostMapping(value = "/addDilyStatus")
 	public void createDailyStatus(@RequestBody PlanDetailFormData formData) {
 		bcmService.createDilyStatus(formData);
-		
+
 	}
 	/*
 	 * getAllAccounts
 	 */
-	
+
 	@GetMapping(value = "/getAllAccounts")
-	public ResponseEntity<List<Account>> getAllAccount(){
+	public ResponseEntity<List<Account>> getAllAccount() {
 		List<Account> accountList = accountRepo.findAll();
 		return ResponseEntity.ok().body(accountList);
 	}
+
 	/*
 	 * getLeader
 	 */
 	@GetMapping(value = "/getLeader/{accountId}")
-	public Leader getLeaderById(@PathVariable("accountId") int accountId){
+	public Leader getLeaderById(@PathVariable("accountId") int accountId) {
 		List<ProjLocLeaderAssoc> assocList = asscoRepo.findAll();
-		for(ProjLocLeaderAssoc leaderAssoc:assocList){
-			if(leaderAssoc.getLeader().getId() == accountId){
+		for (ProjLocLeaderAssoc leaderAssoc : assocList) {
+			if (leaderAssoc.getLeader().getId() == accountId) {
 				return leaderAssoc.getLeader();
 			}
 		}
 		return null;
 	}
+
 	/*
 	 * getProject
 	 */
 	@GetMapping(value = "/getProject/{accountId}")
-	public ResponseEntity<List<Project>> getProject(@PathVariable("accountId") int accountId){
+	public ResponseEntity<List<Project>> getProject(@PathVariable("accountId") int accountId) {
 		List<ProjLocLeaderAssoc> assocList = projecAssocRepo.findAll();
 		List<Project> projList = new ArrayList<Project>();
-		for(ProjLocLeaderAssoc projAssoc:assocList){
-			if(projAssoc.getAccount().getId() == accountId){
+		for (ProjLocLeaderAssoc projAssoc : assocList) {
+			if (projAssoc.getAccount().getId() == accountId) {
 				projList.add(projAssoc.getProject());
 			}
 		}
 		return ResponseEntity.ok().body(projList);
 	}
+
 	/*
 	 * getAllLocation
 	 */
 	@GetMapping(value = "/getAllLocation")
-	public ResponseEntity<List<Location>>getAllLocation(){
-		List<Location> locatonList=locationRepo.findAll();
+	public ResponseEntity<List<Location>> getAllLocation() {
+		List<Location> locatonList = locationRepo.findAll();
 		return ResponseEntity.ok().body(locatonList);
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
 
 }
--- src/main/java/com/altimetrik/bcp/model/PlanDetailFormData.java
+++ src/main/java/com/altimetrik/bcp/model/PlanDetailFormData.java
@@ -2,119 +2,155 @@ package com.altimetrik.bcp.model;
 
 import java.util.Date;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 public class PlanDetailFormData {
-	Date date;
-	int locationId;
-	int accountId;
-	int leaderId;
-	int projectId;
-	String status;
-	int teamSize;
-	int loogedCount;
-	String DeliveryOnTrack;
-	int targetPercentage;
-	int actualPercentage;
-	String milestone;
-	String deliveryChallenge;
-	String mitigationPlan;
-	String wfhChallenge;
-	String keyDeliverable;
+	@DateTimeFormat(pattern = "yyyy-mm-dd")
+	private Date date;
+	private int locationId;
+	private int accountId;
+	private int leaderId;
+	private int projectId;
+	private String status;
+	private int teamSize;
+	private int loogedCount;
+	private String DeliveryOnTrack;
+	private int targetPercentage;
+	private int actualPercentage;
+	private String milestone;
+	private String deliveryChallenge;
+	private String mitigationPlan;
+	private String wfhChallenge;
+	private String keyDeliverable;
+
 	public int getAccountId() {
 		return accountId;
 	}
+
 	public void setAccountId(int accountId) {
 		this.accountId = accountId;
 	}
+
 	public Date getDate() {
 		return date;
 	}
+
 	public void setDate(Date date) {
 		this.date = date;
 	}
+
 	public int getLocationId() {
 		return locationId;
 	}
+
 	public void setLocationId(int locationId) {
 		this.locationId = locationId;
 	}
+
 	public int getLeaderId() {
 		return leaderId;
 	}
+
 	public void setLeaderId(int leaderId) {
 		this.leaderId = leaderId;
 	}
+
 	public int getProjectId() {
 		return projectId;
 	}
+
 	public void setProjectId(int projectId) {
 		this.projectId = projectId;
 	}
+
 	public String getStatus() {
 		return status;
 	}
+
 	public void setStatus(String status) {
 		this.status = status;
 	}
+
 	public int getTeamSize() {
 		return teamSize;
 	}
+
 	public void setTeamSize(int teamSize) {
 		this.teamSize = teamSize;
 	}
+
 	public int getLoogedCount() {
 		return loogedCount;
 	}
+
 	public void setLoogedCount(int loogedCount) {
 		this.loogedCount = loogedCount;
 	}
+
 	public String getDeliveryOnTrack() {
 		return DeliveryOnTrack;
 	}
+
 	public void setDeliveryOnTrack(String deliveryOnTrack) {
 		DeliveryOnTrack = deliveryOnTrack;
 	}
+
 	public int getTargetPercentage() {
 		return targetPercentage;
 	}
+
 	public void setTargetPercentage(int targetPercentage) {
 		this.targetPercentage = targetPercentage;
 	}
+
 	public int getActualPercentage() {
 		return actualPercentage;
 	}
+
 	public void setActualPercentage(int actualPercentage) {
 		this.actualPercentage = actualPercentage;
 	}
+
 	public String getMilestone() {
 		return milestone;
 	}
+
 	public void setMilestone(String milestone) {
 		this.milestone = milestone;
 	}
+
 	public String getDeliveryChallenge() {
 		return deliveryChallenge;
 	}
+
 	public void setDeliveryChallenge(String deliveryChallenge) {
 		this.deliveryChallenge = deliveryChallenge;
 	}
+
 	public String getMitigationPlan() {
 		return mitigationPlan;
 	}
+
 	public void setMitigationPlan(String mitigationPlan) {
 		this.mitigationPlan = mitigationPlan;
 	}
+
 	public String getWfhChallenge() {
 		return wfhChallenge;
 	}
+
 	public void setWfhChallenge(String wfhChallenge) {
 		this.wfhChallenge = wfhChallenge;
 	}
+
 	public String getKeyDeliverable() {
 		return keyDeliverable;
 	}
+
 	public void setKeyDeliverable(String keyDeliverable) {
 		this.keyDeliverable = keyDeliverable;
 	}
+
 	@Override
 	public String toString() {
 		StringBuilder builder = new StringBuilder();
@@ -153,5 +189,5 @@ public class PlanDetailFormData {
 		builder.append("]");
 		return builder.toString();
 	}
-	
+
 }
